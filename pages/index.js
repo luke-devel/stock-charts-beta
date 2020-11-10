5810afcb5a5e619d66428ead26874d54b3671b4e
luke <luke.olson499@gmail.com>
2020-11-10 14:48:13 -0600

    init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChartComponent from '../components/ChartComponent'
+
+export default function HomePage() {
+	return (
+		<div>
+			<ChartComponent />
+		</div >
+	)
+}
